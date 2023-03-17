b12932ef2f0ac59f2f5f46640b54564308db3fa8
marcocnc <marcocancelliere56@gmail.com>
2023-03-17 14:59:02 +0100

    created password

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@
 4. Stampare su schermo la password generata che sarà tipo nomecognomecolorepreferito23.
 */
 
+const nameUser = prompt('Qual è il tuo nome?');
+const surnameUser = prompt('E il tuo cognome?');
+const colorUser = prompt('Ora dimmi il tuo colore preferito.');
+
+console.log(nameUser, surnameUser, colorUser);
+
+const password = nameUser + surnameUser + colorUser;
+console.log(password);
