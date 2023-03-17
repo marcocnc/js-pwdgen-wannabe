0cea57f0540a9588c434ce6ebe49f69ca2a6c708
marcocnc <marcocancelliere56@gmail.com>
2023-03-17 14:51:26 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* Consegna esercizio:
+1. Chiedere il nome all'utente;
+2. Chiedere cognome;
+3. Chiedere colore preferito;
+4. Stampare su schermo la password generata che sarà tipo nomecognomecolorepreferito23.
+*/
+
